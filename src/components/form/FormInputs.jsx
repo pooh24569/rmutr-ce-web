9553f44a3b8cd0dd661e18c75c4d0cdef48d46c7
Pooh <1651010541132@rmutr.ac.th>
2025-07-09 01:18:31 +0700

    first commit

---
+++ src/components/form/FormInputs.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+
+const FormInputs = ({ register, name, type, placeholder }) => {
+  return (
+    <div className="mb-2">
+      <Label htmlFor={name} className="capitalize">
+        {name}
+      </Label>
+      <Input
+        {...register(name)}
+        type={type}
+        name="title"
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default FormInputs;
